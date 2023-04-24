d1e51b52ade8706a1efa7acad992d2d600ab8f40
Admin <subashbaidya9@gmail.com>
2023-04-24 11:03:35 +0545

    First Animation

---
+++ src/components/GettingStarted.js
@@ -0,0 +1,30 @@
+import { animated, useSpring } from "@react-spring/web";
+import React from 'react'
+
+export default function GettingStarted() {
+    const [springs, api] = useSpring(()=>({
+        from: {x: 0},
+    }))
+    const handleClick = () =>{
+        api.start({
+            from: {
+                x:0,
+            },
+            to:{
+                x:100,
+            },
+        })
+    }
+  return (
+    <animated.div 
+        onClick={handleClick}
+        style={{
+            width:80,
+            height:80,
+            background: '#ff6d6d',
+            borderRadius: 8,
+            ...springs,
+        }}
+    />
+  )
+}
