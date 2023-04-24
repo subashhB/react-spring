6c9b19d182bad6de28c591622a8c45946285c6a2
Admin <subashbaidya9@gmail.com>
2023-04-24 18:27:39 +0545

    Skydiving Cat

--- src/pages/ParallaxPage.js
+++ src/pages/ParallaxPage.js
@@ -1,20 +1,53 @@
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import React from 'react'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import React, { useRef } from "react";
+import moon from "./moon.png";
+import land from "./land.png";
+import cat from "./cat.gif";
 
 const ParallaxPage = () => {
-    
+  const ref = useRef();
   return (
-    <div style={{display: 'flex',justifyContent: 'center', margin: 'auto'}}>
-      <Parallax pages={4}>
-        <ParallaxLayer speed={1}>
-          <h1>Welcome to the Parallax Page.</h1>
+    <div style={{ display: "flex", justifyContent: "center", margin: "auto" }}>
+      <Parallax pages={4} ref={ref}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: "cover",
+          }}
+        />
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{ backgroundImage: `url(${land})`, backgroundSize: "cover" }}
+        />
+        <ParallaxLayer
+        sticky={{start:0.9, end: 2.5}}
+          style={{textAlign: 'center'}}
+        >
+          <img src={cat} alt="Skydiving Cat"/>
         </ParallaxLayer>
-        <ParallaxLayer speed={0.5} offset={1}>
-          <h1>Web Developmnet was supposed to be fun.</h1>
+        <ParallaxLayer
+          offset={0.1}
+          speed={0.05}
+          style={{fontSize: '0.3em'}}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Web Development was supposed to be fun.</h2>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={()=>ref.current.scrollTo(0)}
+        >
+          <h2>Hello Visitor</h2>
         </ParallaxLayer>
       </Parallax>
     </div>
-  )
-}
+  );
+};
 
-export default ParallaxPage
+export default ParallaxPage;
